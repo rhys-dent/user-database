3a83e48f2551ac1047b9d7e87dea2058f9d18bbc
Rhys Dent <devRhysDent@gmail.com>
2021-07-25 17:04:15 -0600

    fixed script

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_URL, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-});
-const db = mongoose.connection;
-db.on("error", (error) => console.log("error"));
-db.once("open", () => console.log("Connected to database"));
+// mongoose.connect(process.env.DATABASE_URL, {
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// });
+// const db = mongoose.connection;
+// db.on("error", (error) => console.log("error"));
+// db.once("open", () => console.log("Connected to database"));
 
 app.use(express.json());
 
