82734cd867acc7f9cc4ec2a1f8fea6dd65506d2a
Rhys Dent <devRhysDent@gmail.com>
2021-07-25 17:13:10 -0600

    added env PORT

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-// mongoose.connect(process.env.DATABASE_URL, {
-// 	useNewUrlParser: true,
-// 	useUnifiedTopology: true,
-// });
-// const db = mongoose.connection;
-// db.on("error", (error) => console.log("error"));
-// db.once("open", () => console.log("Connected to database"));
+mongoose.connect(process.env.DATABASE_URL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.log("error"));
+db.once("open", () => console.log("Connected to database"));
 
 app.use(express.json());
 
@@ -18,4 +18,6 @@ app.use("/", express.static("static"));
 const subscribersRouter = require("./routes/subscribers");
 app.use("/subscribers", subscribersRouter);
 
-app.listen(3000, () => console.log("Server running on 3000"));
+app.listen(process.env.PORT || 3000, () =>
+	console.log("Server running on 3000")
+);
